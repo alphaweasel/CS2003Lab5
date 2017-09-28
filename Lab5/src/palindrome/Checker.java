c1ec587c037d5f396f574e91eeda54cc635d0143
Garrison <Garrison@172.16.45.45>
2017-09-28 10:43:10 -0500

    Fixed even length palindromes

--- Lab5/src/palindrome/Checker.java
+++ Lab5/src/palindrome/Checker.java
@@ -11,10 +11,16 @@ public class Checker{
 	}
 	
 	public static boolean isaPalindrome(String in) {
+		//base case for words with an odd length
 		if(in.length() == 1)
 			return true;
+		//base case for words with an even length
+		else if(in.length() == 2 && in.charAt(0) == in.charAt(1))
+			return true;
+		//recursion
 		else if(in.charAt(0) == in.charAt(in.length()-1))
 			return isaPalindrome(in.substring(1,in.length()-1));
+		//false if the characters do not pass the equality test
 		else
 			return false;
 	}
